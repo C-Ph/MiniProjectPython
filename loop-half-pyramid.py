238b1b8c45811520d3d58361aa89d096015fca40
LunaTheBoy <126503435+LunaTheBoy@users.noreply.github.com>
2023-05-27 23:04:04 +0700

    Create loop-half-pyramid.py

---
+++ loop-half-pyramid.py
@@ -0,0 +1,16 @@
+x = "*"
+for i in range(5):
+    print("".join(x))
+    x += "*"
+    
+#other examples
+
+#1
+x = "*"
+for i in range(6):
+    print(x)
+    x += "*"
+#2
+for i in range(1, 7):
+    print("*" * i)
+    
