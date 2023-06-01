58237d1f5cf3e2ec164a88f8b42a9ffb57d56184
IX <126503435+C-Ph@users.noreply.github.com>
2023-06-01 21:58:22 +0700

    Update TaskManager.py
    Add the search task first alphabetically. Include both uppercase and lowercase letters.

--- TaskManager.py
+++ TaskManager.py
@@ -1,10 +1,7 @@
 tasks = []
 
-def frint():
-    print("================")
 
-
-def add_task(task):
+def add_task(task): #parameters
     tasks.append(task)
     print("Task added successfully.")
 
@@ -12,7 +9,7 @@ def add_task(task):
 def view_tasks():
     if tasks:
         print("Tasks:")
-        frint()
+        print("========================")
         for index, task in enumerate(tasks, start=1):
             print(f"{index}. {task}")
             """if tasks: check the "tasks" list is not empty.
@@ -21,12 +18,32 @@ def view_tasks():
             enumerate(collection-name, start=0(optional))
             f-string(formatted string)"""
     else:
-        frint()
+        print("========================")
         print("No tasks found.")
-    frint()
+    print("========================")
+
+def search_tasks():
+    searhch_results = []
+    for add_searhchlist in tasks:
+        if add_searhchlist.lower().startswith(searhchs.lower()):
+            """startswith() มาช่วยเปรียบเทียบจริงเท็จว่า
+            add_searhchlist.lower()ขึ้นต้นด้วยsearhchs.lower()ไหมใช่จะทำงาน"""
+            searhch_results.append(add_searhchlist)
+
+    if searhch_results:
+        print("Tasks starting with the letter", searhchs + ":")
+        #+เชื่อมstringให้ติดกันไม่มีเว้นวรรค
+        print("========================")
+        for index, searhch_results in enumerate(searhch_results, start=1):
+            print(f"{index}. {searhch_results}")
+            """ Or for result in searhch_results:
+            print(result) """
+        print("========================")
+    else:
+        print("No tasks found starting with the letter", searhchs)
 
 
-def delete_task(task_index):
+def delete_task(task_index): #parameters
     if task_index >= 1 and task_index <= len(tasks):
         delete = tasks.pop(task_index - 1)
         print(f"Deleted task: {delete}")
@@ -43,22 +60,27 @@ while True:
     print("\n===== Task Manager =====")
     print("1. Add Task")
     print("2. View Tasks")
-    print("3. Delete Task")
-    print("4. Quit")
+    print("3. Search Tasks")
+    print("4. Delete Task")
+    print("5. Quit")
+    print("========================\n")
 
-    choice = input("Enter your choice (1-4): ")
+    choice = input("Enter your choice (1-5): ")
 
     if choice == "1":
         task = input("Enter task: ")
-        add_task(task)
+        add_task(task) #argument
         #Append the task to the tasks list,Is not function to return
     elif choice == "2":
         view_tasks()
     elif choice == "3":
-        task_index = int(input("Enter task index to delete: "))
-        delete_task(task_index)
+        searhchs = input("Enter an alphabet letter : ")
+        search_tasks()
     elif choice == "4":
-        print("Exiting Task Manager...")
+        task_index = int(input("Enter task index to delete: "))
+        delete_task(task_index) #argument
+    elif choice == "5":
+        print("See you later, Thankyou!!")
         break
     else:
         print("Invalid choice. Please try again.")
